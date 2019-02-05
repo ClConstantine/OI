92b78c2feb1db39a37ef2a5aea5f94012afcb668
ClConstantine <40931032+ClConstantine@users.noreply.github.com>
2019-02-05 22:06:55 +0800

    入门 储存

---
+++ Luogu/p1427.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int n;
+int Ican[100010];
+int main(){
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&Ican[i]);
+    }
+    sort(Ican,Ican+n);
+    for(int i=0;i<n;i++)
+        printf("%d ",Ican[i]);
+    return 0;
+}
