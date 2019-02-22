0ca70db1f376be62ef2764e0ed27d9c8b56dd81c
ClConstantine <40931032+ClConstantine@users.noreply.github.com>
2019-02-22 21:36:04 +0800

    普及 Long

---
+++ Luogu/p2394.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+using std::cin; using std::cout; using std::endl; using std::cerr;
+
+typedef long double LD;
+
+int main(){
+    cin.tie(0);
+    std::ios::sync_with_stdio(false);
+
+    LD temp;
+    scanf("%17Lf",&temp);
+    printf("%.8Lf",temp/23);
+
+    return 0;
+}
