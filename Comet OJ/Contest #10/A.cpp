88301be29d53335939799669086b66e0982e28d2
ClConstantine <yck1901054815@126.com>
2019-09-20 22:32:37 +0800

    Create A.cpp

---
+++ Comet OJ/Contest #10/A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+using std::cin;using std::cerr;using std::cout;using std::endl;
+
+char s[1000+10];
+char st[10];
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n,len,ans=0;
+
+    cin>>n;
+
+    st[1]='a';st[2]='c';st[3]='p';st[4]='t';st[5]='y';
+
+    while(n--){
+        int i;
+        cin>>s+1;
+        len=strlen(s+1);
+        for(i=1;i<=5;i++){
+            if(s[len-5+i]!=st[i]) break;
+        }
+        if(i==6) ans++;
+    }
+
+    cout<<ans;
+
+    return 0;
+}
