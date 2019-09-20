c067cc7e1254edb16bfca11b78042b5b18f5a6ae
ClConstantine <yck1901054815@126.com>
2019-09-20 22:39:58 +0800

    模拟

--- Comet OJ/Contest #10/A.cpp
+++ Comet OJ/Contest #10/A.cpp
@@ -9,6 +9,7 @@ using std::cin;using std::cerr;using std::cout;using std::endl;
 char s[1000+10];
 char st[10];
 int main(){
+
     std::ios::sync_with_stdio(false);
     cin.tie(0);
 
@@ -19,7 +20,7 @@ int main(){
     st[1]='a';st[2]='c';st[3]='p';st[4]='t';st[5]='y';
 
     while(n--){
-        int i;
+        int i=0;
         cin>>s+1;
         len=strlen(s+1);
         for(i=1;i<=5;i++){
