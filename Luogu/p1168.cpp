0b0c3cba7e3f7b73caac2b1a0a9cfa456226b841
ClConstantine <40931032+ClConstantine@users.noreply.github.com>
2019-12-20 15:04:31 +0800

    提高 vector妙用

--- Luogu/p1168.cpp
+++ Luogu/p1168.cpp
@@ -12,10 +12,12 @@ int main()
     cin>>n>>t;
     s.push_back(t);
     cout<<t<<endl;
+    
     for(int i=2;i<=n;i++){
         cin>>t;
         s.insert(lower_bound(s.begin(),s.end(),t),t);
         if(i&1) cout<<s[i/2]<<endl;
     }
+    
     return 0;
 }
