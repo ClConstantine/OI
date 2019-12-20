06ce88d955cade3fedd50c9d68feac131277f21d
ClConstantine <40931032+ClConstantine@users.noreply.github.com>
2019-12-20 15:03:35 +0800

    提高 vec投入

---
+++ Luogu/p1168.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+vector<int> s;
+
+int main()
+{
+    int n,t;
+    cin>>n>>t;
+    s.push_back(t);
+    cout<<t<<endl;
+    for(int i=2;i<=n;i++){
+        cin>>t;
+        s.insert(lower_bound(s.begin(),s.end(),t),t);
+        if(i&1) cout<<s[i/2]<<endl;
+    }
+    return 0;
+}
