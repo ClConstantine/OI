a77600daf12ac6ed555b8e42d137d69ef70daf60
ClConstantine <yck1901054815@126.com>
2020-03-12 12:28:40 +0800

    Create c1023.cpp

---
+++ Comet OJ/c1023.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using std::cin;using std::cout;using std::endl;using std::cerr;
+
+int main(){
+    long long N;
+
+    cin>>N;
+
+    cout<<(N+1)*N-N;
+    
+    return 0;
+}
